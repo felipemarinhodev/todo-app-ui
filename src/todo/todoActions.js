15ea316d4f4cfc7940143a78fe35ad9c4561f20a
Felipe Marinho <felipmarinho@gmail.com>
2020-04-11 06:49:37 -0300

    92 - Clean form

--- src/todo/todoActions.js
+++ src/todo/todoActions.js
@@ -18,7 +18,7 @@ export const search = () => {
 export const add = (description) => {
 	return dispatch => {
 		axios.post(URL, { description })
-			.then(resp => dispatch({ type: 'TODO_ADDED', payload: resp.data}))
+			.then(resp => dispatch(clear()))
 			.then(() => dispatch(search()))
 	}
 }
@@ -42,5 +42,8 @@ export const remove = (todo) => {
 		axios.delete(`${URL}/${todo._id}`)
 		.then(() => dispatch(search()))
 	}
+}
 
+export const clear = (todo) => {
+	return { type: 'TODO_CLEAR'	}
 }
